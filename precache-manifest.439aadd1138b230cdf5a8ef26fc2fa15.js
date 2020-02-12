56700c83910e6f6f743c6f09d7436cce7a8ce788
duonghoangdung <dung.duonghoang94@gmail.com>
2020-02-12 14:26:22 +0700

    Updates

---
+++ precache-manifest.439aadd1138b230cdf5a8ef26fc2fa15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e736c4df0ac2cf1e6225e6ca9d60c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4d3960950aea33287ac",
+    "url": "/static/css/main.acbe0b60.chunk.css"
+  },
+  {
+    "revision": "f715d30618a1666398ca",
+    "url": "/static/js/2.704e426c.chunk.js"
+  },
+  {
+    "revision": "b4d3960950aea33287ac",
+    "url": "/static/js/main.eda85a71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
